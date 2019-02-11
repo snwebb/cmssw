18cf1eb3b7192c66abe47b9fdc67c1c93a385161
Samuel Webb <samuel.webb@cern.ch>
2019-02-11 10:31:07 +0100

    Cleaning code for pull request

--- L1Trigger/L1THGCal/interface/backend/HGCalMulticlusteringHistoImpl.h
+++ L1Trigger/L1THGCal/interface/backend/HGCalMulticlusteringHistoImpl.h
@@ -36,6 +36,7 @@ public:
 private:
     enum MulticlusterType{
       HistoMaxC3d,
+      HistoSecondaryMaxC3d,
       HistoThresholdC3d,
       HistoInterpolatedMaxC3d
     };
@@ -51,7 +52,7 @@ private:
 
     std::vector<std::pair<GlobalPoint, double> > computeMaxSeeds( const Histogram & histoClusters );
 
-    std::vector<std::pair<GlobalPoint, double> > computeModifiedMaxSeeds( const Histogram & histoClusters );
+    std::vector<std::pair<GlobalPoint, double> > computeSecondaryMaxSeeds( const Histogram & histoClusters );
     
     std::vector<std::pair<GlobalPoint, double> > computeInterpolatedMaxSeeds( const Histogram & histoClusters );
     
