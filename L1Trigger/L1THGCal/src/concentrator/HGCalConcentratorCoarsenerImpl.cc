f30f0f981daeba78d72a2dd85104e01db5086bce
Samuel Webb <samuel.webb@cern.ch>
2019-05-22 14:36:01 +0200

    Remove erroneous code in Coarsener

--- L1Trigger/L1THGCal/src/concentrator/HGCalConcentratorCoarsenerImpl.cc
+++ L1Trigger/L1THGCal/src/concentrator/HGCalConcentratorCoarsenerImpl.cc
@@ -52,36 +52,14 @@ coarsen(const std::vector<l1t::HGCalTriggerCell>& trigCellVecInput, std::vector<
     updateCoarseTriggerCellMaps( tc, ctcid );
   }
 
-
-
-  for (const l1t::HGCalTriggerCell & tc : trigCellVecInput) {
-
-    int thickness = triggerTools_.thicknessIndex(tc.detId(),true);
-    if ( fixedDataSizePerHGCROC_ && thickness == kHighDensityThickness_ ){
-      continue;
-    }
-
-    uint32_t ctcid = coarseTCmapping_.getCoarseTriggerCellId(tc.detId());
-    if ( tc.detId() == coarseTCs_[ctcid].maxId){
-      trigCellVecOutput.push_back( tc );
-      assignCoarseTriggerCellEnergy( trigCellVecOutput.back(), ctcid );
-      coarseTCmapping_.setEvenDetId(trigCellVecOutput.back());
-      GlobalPoint point = coarseTCmapping_.getCoarseTriggerCellPosition( trigCellVecOutput.back().detId() );
-      math::PtEtaPhiMLorentzVector p4(trigCellVecOutput.back().pt(), point.eta(), point.phi(), trigCellVecOutput.back().mass());
-      trigCellVecOutput.back().setPosition(point);
-      trigCellVecOutput.back().setP4(p4);
-
-    }
-    
-  }
-
-
   for (auto & ctc : coarseTCs_){
    l1t::HGCalTriggerCell triggerCell;   
    assignCoarseTriggerCellEnergy( triggerCell, ctc.first );
    coarseTCmapping_.setEvenDetId( triggerCell );        
    GlobalPoint point = coarseTCmapping_.getCoarseTriggerCellPosition( trigCellVecOutput.back().detId() );
+   math::PtEtaPhiMLorentzVector p4( triggerCell.pt(), point.eta(), point.phi(), triggerCell.mass());
    triggerCell.setPosition(point);
+   triggerCell.setP4(p4);
    trigCellVecOutput.push_back( triggerCell );
   }
 
